a43d3775ce39231521854f09e4cf3593d9fde7d6
Amber Hall <94423627+rv-amberh@users.noreply.github.com>
2022-11-07 09:22:26 -0800

    Update products.js

--- products.js
+++ products.js
@@ -10,36 +10,7 @@ toggle.addEventListener("click", () => {
   navigation.classList.toggle("active");
 });
 
-/*
-    We start our code with an ajax request to fetch the data
-    from the json file.
-*/
-// First i create a new xmlhttp-request object.
-// let http = new XMLHttpRequest();
-// the variable http holds now all methods and properties of the objct.
-
-//  next i prepare the request with the open() method.
-//  http.open('get', 'products.json', true);
-// the first argument sets the http method.
-// in the second argument we pass the file where our data lives.
-// and last the keyword true, sets the request to be async.
-
-// next i will send the request.
-// http.send();
-
-// Now i have to catch the response.
-// i will check the onload eventlistener.
-//http.onload = function(){
-// Inside the function i need to check the readystate and status properties.
-//if(this.readyState == 4 && this.status == 200){
-// if we have a successful response, i have to parse the json data
-// and convert them to a javascript array.
-//let products = JSON.parse(this.responseText);
-
-// next i need an empty variable to add the incoming data.
-//fetc/('http://localhost:8000/products')//
-   //   .then(resp => resp.json())//
-   //     .then(data => showProducts(data));//
+//Hardcoded products since we are not running this with my local db. 
 
 
 products = [
